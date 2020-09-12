6cc3f10a09dbd84f68d0ee7df8da43ff6535e375
yoyobbs <79379450@qq.com>
2020-09-13 01:34:35 +0800

    Update v2xy.sh

--- v2xy.sh
+++ v2xy.sh
@@ -249,7 +249,7 @@ config_caddy_docker(){
     cd ${cur_dir}
     echo "install curl"
     install_dependencies
-    curl -L https://raw.githubusercontent.com/xiaoyanggo/v2rayshell/master/Caddyfile >  Caddyfile
+    curl -L https://raw.githubusercontent.com/yoyobbs/yml/master/Caddyfile >  Caddyfile
     echo "Writing docker-compose.yml"
     curl -L https://raw.githubusercontent.com/yoyobbs/yml/master/docker-compose.yml > docker-compose.yml
     sed -i "s|node_id:.*|node_id: ${ssrpanel_node_id}|"  ./docker-compose.yml
